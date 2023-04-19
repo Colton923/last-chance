21e8449bfbb3014b2aa1d74a550378fe32877f37
Colton923 <110353948+Colton923@users.noreply.github.com>
2023-04-19 12:06:54 -0400

    donation request form

---
+++ app/api/sendgridDonation/route.ts
@@ -0,0 +1,98 @@
+import { MailService } from '@sendgrid/mail'
+import type { DonationForm } from 'app/donation/page'
+import { NextResponse } from 'next/server'
+
+export async function POST(request: Request) {
+  const body: DonationForm = await request.json()
+  if (!process.env.SENDGRID_API_KEY)
+    throw new Error('SENDGRID_API_KEY is not defined.')
+
+  const mailService = new MailService()
+  mailService.setApiKey(process.env.SENDGRID_API_KEY as string)
+
+  const msg = {
+    to:
+      process.env.NEXT_PUBLIC_ENVIRONMENT === 'development'
+        ? 'colton923@gmail.com'
+        : 'lastchancebarandgrill@yahoo.com',
+    from: 'lastchancewebapp@gmail.com',
+    subject: `New Donation Request from ${body.contactName}`,
+    text: `
+    `,
+    html: `
+      <h1>New Donation Request</h1>
+      <p><strong>Contact Name:</strong> ${
+        body.contactName ? body.contactName : ''
+      }</p>
+      <p><strong>Contact Email:</strong> ${
+        body.contactEmail ? body.contactEmail : ''
+      }</p>
+      <p><strong>Contact Phone:</strong> ${
+        body.contactPhone ? body.contactPhone : ''
+      }</p>
+      <p><strong>Organization Name:</strong> ${
+        body.organizationName ? body.organizationName : ''
+      }</p>
+      <p><strong>Organization Address:</strong> ${
+        body.contactAddress ? body.contactAddress : ''
+      }</p>
+      <p><strong>Organization City:</strong> ${
+        body.contactCity ? body.contactCity : ''
+      }</p>
+      <p><strong>Organization State:</strong> ${
+        body.contactState ? body.contactState : ''
+      }</p>
+      <p><strong>Organization Zip:</strong> ${
+        body.contactZip ? body.contactZip : ''
+      }</p>
+      <p><strong>Organization Donation Needed By:</strong> ${
+        body.donationNeededBy ? body.donationNeededBy : ''
+      }</p>
+      <p><strong>Organization Donation Value:</strong> ${
+        body.donationValue ? body.donationValue : ''
+      }</p>
+      <p><strong>Organization Event Date:</strong> ${
+        body.eventDate.start ? body.eventDate.start : ''
+      } - ${body.eventDate.end ? body.eventDate.end : ''}</p>
+      <p><strong>Organization Event Purpose:</strong> ${
+        body.eventPurpose ? body.eventPurpose : ''
+      }</p>
+      <p><strong>Organization Event Type:</strong> ${
+        body.eventType ? body.eventType : ''
+      }</p>
+      <p><strong>Organization Participants:</strong> ${
+        body.participants ? body.participants : ''
+      }</p>
+      <p><strong>Organization Donation Description:</strong> ${
+        body.requestDate ? body.requestDate : ''
+      }</p>
+      <p><strong>Organization Donation Type:</strong> ${
+        body.typeOfDonation ? body.typeOfDonation : ''
+      }</p>
+      <p><strong>Organization Why The Last Chance:</strong> ${
+        body.whyUs ? body.whyUs : ''
+      }</p>
+    `,
+  }
+
+  try {
+    mailService.send(msg)
+    return NextResponse.json(
+      {
+        message: 'Donation Request Submitted',
+      },
+      {
+        status: 200,
+      }
+    )
+  } catch (error) {
+    return NextResponse.json(
+      {
+        message: 'Donation Request Failed to Send',
+      },
+      {
+        status: 500,
+      }
+    )
+  }
+}
--- app/donation/page.tsx
+++ app/donation/page.tsx
@@ -1,5 +1,274 @@
-import styles from 'styles/App.module.scss'
+'use client'
+
+import { useForm } from 'react-hook-form'
+import styles from './Donation.module.scss'
+
+export type DonationForm = {
+  requestDate: string
+  donationNeededBy: string
+  organizationName: string
+  eventDate: {
+    start: string
+    end: string
+  }
+  eventType:
+    | 'School Support'
+    | 'Non-Profit Support'
+    | 'Athletic Boosters'
+    | 'Military'
+    | 'Benefit'
+    | 'Sporting Event/Tournament'
+    | 'Other'
+  participants: number
+  eventPurpose: string
+  typeOfDonation: string
+  donationValue: number
+  whyUs: string
+  contactName: string
+  contactEmail: string
+  contactPhone: string
+  contactAddress: string
+  contactCity: string
+  contactState: string
+  contactZip: string
+}
 
 export default function Donation() {
-  return <div className={styles.wrapper}>Donation Request Ashley Smells</div>
+  const { register, handleSubmit } = useForm<DonationForm>()
+
+  const onSubmit = async (data: DonationForm) => {
+    const body: DonationForm = {
+      requestDate: data.requestDate,
+      donationNeededBy: data.donationNeededBy,
+      organizationName: data.organizationName,
+      eventDate: {
+        start: data.eventDate.start,
+        end: data.eventDate.end,
+      },
+      eventType: data.eventType,
+      participants: data.participants,
+      eventPurpose: data.eventPurpose,
+      typeOfDonation: data.typeOfDonation,
+      donationValue: data.donationValue,
+      whyUs: data.whyUs,
+      contactName: data.contactName,
+      contactEmail: data.contactEmail,
+      contactPhone: data.contactPhone,
+      contactAddress: data.contactAddress,
+      contactCity: data.contactCity,
+      contactState: data.contactState,
+      contactZip: data.contactZip,
+    }
+    await fetch('/api/sendgridDonation', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(body),
+    })
+      .then((res) => {
+        console.log(res)
+        return res.json()
+      })
+      .then((data) => {
+        console.log(data)
+        alert(data.message)
+      })
+  }
+
+  return (
+    <div className={styles.wrapper}>
+      <h2 className={styles.header}>Donation Request Form</h2>
+      <h3 className={styles.formInstructions}>
+        Please print neatly. Submission of this form does not guarantee a donation.
+        Last Chance management will review your request and contact you with their
+        decision. All requests must be submitted at least 2 weeks in advance.
+      </h3>
+      <div className={styles.formWrapper}>
+        <form onSubmit={handleSubmit(onSubmit)} className={styles.form}>
+          <label className={styles.label}>
+            Request Date:
+            <input
+              type="date"
+              defaultValue={new Date().toISOString().substring(0, 10)}
+              {...register('requestDate')}
+              className={styles.dateInput}
+            />
+          </label>
+          <label className={styles.label}>
+            Donation Needed By:
+            <input
+              type="date"
+              defaultValue={new Date().toISOString().substring(0, 10)}
+              {...register('donationNeededBy')}
+              className={styles.dateInput}
+            />
+          </label>
+          <label className={styles.label}>
+            Organization/Event Name:
+            <input
+              type="text"
+              {...register('organizationName')}
+              defaultValue={'Organization/Event Name'}
+              className={styles.textInput}
+            />
+          </label>
+          <label className={styles.label}>
+            Date(s) of Event:
+            <input
+              type="date"
+              defaultValue={new Date().toISOString().substring(0, 10)}
+              {...register('eventDate.start')}
+              className={styles.dateInput}
+            />
+            <p> to </p>
+            <input
+              type="date"
+              defaultValue={new Date().toISOString().substring(0, 10)}
+              {...register('eventDate.end')}
+              className={styles.dateInput}
+            />
+          </label>
+          <label className={styles.label}>
+            Type of Event:
+            <select {...register('eventType')} className={styles.selectInput}>
+              <option className={styles.optionInput} value="School Support">
+                School Support
+              </option>
+              <option className={styles.optionInput} value="Non-Profit Support">
+                Non-Profit Support
+              </option>
+              <option className={styles.optionInput} value="Athletic Boosters">
+                Athletic Boosters
+              </option>
+              <option className={styles.optionInput} value="Military">
+                Military
+              </option>
+              <option className={styles.optionInput} value="Benefit">
+                Benefit
+              </option>
+              <option
+                className={styles.optionInput}
+                value="Sporting Event/Tournament"
+              >
+                Sporting Event/Tournament
+              </option>
+              <option className={styles.optionInput} value="Other">
+                Other
+              </option>
+            </select>
+          </label>
+          <label className={styles.label}>
+            Number of Participants:
+            <input
+              className={styles.numberInput}
+              type="number"
+              {...register('participants')}
+              defaultValue={0}
+            />
+          </label>
+          <label className={styles.label}>
+            Purpose of Event:
+            <input
+              type="text"
+              {...register('eventPurpose')}
+              defaultValue={'Purpose of Event'}
+              className={styles.textInput}
+            />
+          </label>
+          <label className={styles.label}>
+            Type of Donation Requested:
+            <input
+              type="text"
+              {...register('typeOfDonation')}
+              defaultValue={'Type of Donation Requested'}
+              className={styles.textInput}
+            />
+          </label>
+          <label className={styles.label}>
+            Value of Donation Requested:
+            <input
+              type="number"
+              {...register('donationValue')}
+              defaultValue={0}
+              className={styles.numberInput}
+            />
+          </label>
+          <label className={styles.label}>
+            Why Last Chance?
+            <input
+              type="text"
+              {...register('whyUs')}
+              defaultValue={'Why Last Chance?'}
+              className={styles.textInput}
+            />
+          </label>
+          <label className={styles.label}>
+            Contact Name:
+            <input
+              type="text"
+              {...register('contactName')}
+              defaultValue={'Contact Name'}
+              className={styles.textInput}
+            />
+          </label>
+          <label className={styles.label}>
+            Contact Email:
+            <input
+              type="email"
+              {...register('contactEmail')}
+              defaultValue={'Contact Email'}
+              className={styles.textInput}
+            />
+          </label>
+          <label className={styles.label}>
+            Contact Phone:
+            <input
+              type="tel"
+              {...register('contactPhone')}
+              defaultValue={'Contact Phone'}
+              className={styles.telInput}
+            />
+          </label>
+          <label className={styles.label}>
+            Contact Address:
+            <input
+              type="text"
+              {...register('contactAddress')}
+              defaultValue={'Contact Address'}
+              className={styles.textInput}
+            />
+          </label>
+          <label className={styles.label}>
+            Contact City:
+            <input
+              type="text"
+              {...register('contactCity')}
+              defaultValue={'Contact City'}
+              className={styles.textInput}
+            />
+          </label>
+          <label className={styles.label}>
+            Contact State:
+            <input
+              type="text"
+              {...register('contactState')}
+              defaultValue={'Contact State'}
+              className={styles.textInput}
+            />
+          </label>
+          <label className={styles.label}>
+            Contact Zip:
+            <input
+              type="text"
+              {...register('contactZip')}
+              defaultValue={'Contact Zip'}
+              className={styles.textInput}
+            />
+          </label>
+          <input type="submit" className={styles.submitInput} value={'Submit'} />
+        </form>
+      </div>
+    </div>
+  )
 }
